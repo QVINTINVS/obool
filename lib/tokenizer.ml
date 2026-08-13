c18c02770275df409e5e6e49383cfde39f772eae
oQuintino <matheus.quintinodasilva0@gmail.com>
2026-08-13 07:03:54 -0300

    refactor(tokenizer): use opened lexer token

--- lib/tokenizer.ml
+++ lib/tokenizer.ml
@@ -3,7 +3,7 @@ open Lexer
 let tokenize input =
   let lexbuf = Lexing.from_string input in
   let rec loop acc =
-    match Lexer.token lexbuf with
+    match token lexbuf with
     | Parser.EOF -> List.rev acc
     | token -> loop (token :: acc)
   in
