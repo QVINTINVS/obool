1f0dbad02c5adc4e8c675dfb3091aa983a3cbb94
oQuintino <matheus.quintinodasilva0@gmail.com>
2026-08-13 06:55:37 -0300

    feat(term): support explicit variable count in of_lsb_ints

--- lib/term.ml
+++ lib/term.ml
@@ -11,9 +11,9 @@ type occurrence =
   | Absent
   | Present
 
-let of_lsb_ints positive negated =
-  { positive_bits = Bitv.of_int_us positive
-  ; negated_bits = Bitv.of_int_us negated
+let of_lsb_ints ?(variable_count = Sys.int_size - 1) positive negated =
+  { positive_bits = Bitv.(of_int_us positive |> get |> init variable_count)
+  ; negated_bits = Bitv.(of_int_us negated |> get |> init variable_count)
   }
 ;;
 
--- lib/term.mli
+++ lib/term.mli
@@ -11,7 +11,7 @@ type occurrence =
   | Absent
   | Present
 
-val of_lsb_ints : int -> int -> t
+val of_lsb_ints : ?variable_count:int -> int -> int -> t
 val common_factor : t -> t -> t
 val normalize : t -> normalized
 val positive_variable_at : t -> int -> occurrence
